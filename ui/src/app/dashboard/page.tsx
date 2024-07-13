4a1d60de52483a3fc17b89a056d5f55b6f448b24
Arthur Soares <arthurpmrs@gmail.com>
2024-07-13 10:49:01 -0300

    chore(frontend): add eslint rocketseat preset

--- ui/src/app/dashboard/page.tsx
+++ ui/src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import classes from './page.module.css'
+
 export default function Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Only admins should access it.</p>
-        </div>
-    )
+  return (
+    <div className={classes.container}>
+      <h1>Dashboard</h1>
+      <p>Only admins should access it.</p>
+    </div>
+  )
 }
