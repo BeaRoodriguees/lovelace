ee0f7a7509fb8e5f3427f8c6c4b0b469a8ff272d
Gabriel Gomes <gomesal2009@gmail.com>
2024-07-23 13:40:14 -0300

    Added problem model

--- api/lovelace/models.py
+++ api/lovelace/models.py
@@ -7,6 +7,21 @@ from sqlalchemy.orm import Mapped, mapped_column, registry
 table_registry = registry()
 
 
+class Status(str, Enum):
+    in_queue = 'in_queue'
+    running = 'running'
+    accepted = 'accepted'
+    wrong_answer = 'wrong_answer'
+    time_limit_exceeded = 'time_limit_exceeded'
+    runtime_error = 'runtime_error'
+
+
+class Language(str, Enum):
+    c = 'c'
+    cpp = 'cpp'
+    python = 'python'
+
+
 class Role(str, Enum):
     admin = 'admin'
     user = 'user'
@@ -32,3 +47,31 @@ class User:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
     )
+
+@table_registry.mapped_as_dataclass
+class Submission:
+    __tablename__ = 'submissions'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    body: Mapped[str] = mapped_column(nullable=False)
+    language: Mapped[Language] = mapped_column()
+    status: Mapped[Status] = mapped_column(
+        default=Status.in_queue
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Problem:
+    __tablename__ = 'problems'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    description: Mapped[str] = mapped_column()
+    entry: Mapped[str] = mapped_column(nullable=False)
+    output: Mapped[str] = mapped_column(nullable=False)
