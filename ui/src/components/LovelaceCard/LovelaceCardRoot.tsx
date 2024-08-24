74de4d831d000c0c1a73065d4990124001e0797e
Arthur Soares <arthurpmrs@gmail.com>
2024-08-24 14:30:47 -0300

    feat: add href option to card root, so it can be clicked

--- ui/src/components/LovelaceCard/LovelaceCardRoot.tsx
+++ ui/src/components/LovelaceCard/LovelaceCardRoot.tsx
@@ -1,19 +1,31 @@
+'use client';
+
 import { Card } from '@mantine/core';
 import classes from './LovelaceCardRoot.module.css';
 import { ReactNode } from 'react';
 import { CardType } from '@/lib/types';
+import { useRouter } from 'next/navigation';
 
 interface LovelaceCardRootProps {
   children: ReactNode;
   type: CardType;
+  href?: string;
 }
 
 export default function LovelaceCardRoot({
   children,
   type,
+  href,
 }: LovelaceCardRootProps) {
+  const { push } = useRouter();
   return (
-    <Card radius="md" className={classes.card} shadow="xs" data-type={type}>
+    <Card
+      onClick={() => (href ? push(href) : null)}
+      radius="md"
+      className={classes.card}
+      shadow="xs"
+      data-type={type}
+    >
       <div className={classes.inner}>{children}</div>
     </Card>
   );
