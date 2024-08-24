76ecec8bdcd7745e286a25a1fde72dfc90a5c1c7
Bea Rodrigues <beatrizroodriguees@hotmail.com>
2024-08-24 10:08:40 -0300

    style: fix height content of 404 page

--- ui/src/components/error/not-found.tsx
+++ ui/src/components/error/not-found.tsx
@@ -4,30 +4,28 @@ import classes from './not-found.module.css';
 
 export function NotFound() {
   return (
-    <>
-      <Container className={classes.root}>
-        <div className={classes.inner}>
-          <Illustration className={classes.image} />
-          <div className={classes.content}>
-            <Title className={classes.title}>Nada a ver aqui!</Title>
-            <Text
-              c="dimmed"
-              size="nd"
-              ta="center"
-              className={classes.description}
-            >
-              A página que você está tentando abrir não existe. Você pode ter
-              errado o endereço, ou a página foi movida a outro URL. Se você
-              achar isso um erro, contate o suporte.
-            </Text>
-            <Group justify="center">
-              <Button variant="gradient" component="a" size="md" href="/">
-                Voltar ao Início
-              </Button>
-            </Group>
-          </div>
+    <Container className={classes.root}>
+      <div className={classes.inner}>
+        <Illustration className={classes.image} />
+        <div className={classes.content}>
+          <Title className={classes.title}>Nada a ver aqui!</Title>
+          <Text
+            c="dimmed"
+            size="nd"
+            ta="center"
+            className={classes.description}
+          >
+            A página que você está tentando abrir não existe. Você pode ter
+            errado o endereço, ou a página foi movida a outro URL. Se você achar
+            isso um erro, contate o suporte.
+          </Text>
+          <Group justify="center">
+            <Button variant="gradient" component="a" size="md" href="/">
+              Voltar ao Início
+            </Button>
+          </Group>
         </div>
-      </Container>
-    </>
+      </div>
+    </Container>
   );
 }
