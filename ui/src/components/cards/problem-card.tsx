562ef19d0fe7bef17cca380df28041eeb8bb6a3e
Arthur Soares <arthurpmrs@gmail.com>
2024-08-26 10:12:24 -0300

    fix: now ProblemCard can accept className and any other prop a div can accept

--- ui/src/components/cards/problem-card.tsx
+++ ui/src/components/cards/problem-card.tsx
@@ -3,12 +3,16 @@ import { ProblemStatus } from '@/lib/types';
 import { CardType, Problem } from '@/lib/types';
 import { LovelaceCard } from '@/components/LovelaceCard';
 
-interface ProblemCardProps {
+interface ProblemCardProps extends React.HTMLAttributes<HTMLDivElement> {
   data: Problem;
   clickable?: boolean;
 }
 
-export default function ProblemCard({ data, clickable }: ProblemCardProps) {
+export default function ProblemCard({
+  data,
+  clickable,
+  ...rest
+}: ProblemCardProps) {
   let cardType;
   let icon = undefined;
 
@@ -31,18 +35,20 @@ export default function ProblemCard({ data, clickable }: ProblemCardProps) {
   const href = clickable ? `/problemset/${data.slug}` : undefined;
 
   return (
-    <LovelaceCard.Root type={cardType} href={href}>
-      <LovelaceCard.Status icon={icon} type={cardType} />
-      <LovelaceCard.Content>
-        <LovelaceCard.Title text={data.title} />
-        <LovelaceCard.Tags tags={data.tags} />
-      </LovelaceCard.Content>
-      <LovelaceCard.Side label={'Difficulty'} text={data.difficulty} />
-      <LovelaceCard.Action
-        icon={IconChevronRight}
-        type={cardType}
-        href={`/problemset/${data.slug}`}
-      />
-    </LovelaceCard.Root>
+    <div {...rest}>
+      <LovelaceCard.Root type={cardType} href={href}>
+        <LovelaceCard.Status icon={icon} type={cardType} />
+        <LovelaceCard.Content>
+          <LovelaceCard.Title text={data.title} />
+          <LovelaceCard.Tags tags={data.tags} />
+        </LovelaceCard.Content>
+        <LovelaceCard.Side label={'Difficulty'} text={data.difficulty} />
+        <LovelaceCard.Action
+          icon={IconChevronRight}
+          type={cardType}
+          href={`/problemset/${data.slug}`}
+        />
+      </LovelaceCard.Root>
+    </div>
   );
 }
