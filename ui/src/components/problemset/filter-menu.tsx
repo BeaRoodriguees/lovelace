d53f629fd8f39870f0f8fa79ede3865849ada8c1
Arthur Soares <arthurpmrs@gmail.com>
2024-09-12 17:33:43 -0300

    feat: add navigation through tab

--- ui/src/components/problemset/filter-menu.tsx
+++ ui/src/components/problemset/filter-menu.tsx
@@ -1,17 +1,21 @@
 'use client';
 
 import { Popover, Button, Text, MultiSelect } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { FocusTrap } from '@mantine/core';
 import { IconFilter } from '@tabler/icons-react';
 import { useState } from 'react';
 import { CloseButton } from '@mantine/core';
 import classes from './filter-menu.module.css';
 
 export default function FilterDropdownManu() {
-  const [opened, setOpened] = useState<boolean>(true);
+  const [opened, setOpened] = useState<boolean>(false);
+  const [active, { toggle }] = useDisclosure(false);
 
   function onMenuClose() {
     console.log('fechando...');
     setOpened(false);
+    toggle();
   }
 
   const problemTags = [
@@ -35,10 +39,16 @@ export default function FilterDropdownManu() {
       shadow="md"
       width={520}
       position="bottom-end"
+      closeOnEscape={false}
     >
       <Popover.Target>
         <Button
-          onClick={() => setOpened((o) => !o)}
+          onClick={() =>
+            setOpened((o) => {
+              toggle();
+              return !o;
+            })
+          }
           leftSection={<IconFilter stroke={2} />}
           variant={'lovelace-secondary'}
         >
@@ -47,52 +57,63 @@ export default function FilterDropdownManu() {
       </Popover.Target>
 
       <Popover.Dropdown className={classes.menu}>
-        <div className={classes.wrapper}>
-          <div className={classes.header}>
-            <Text size="lg" fw={700} className={classes.title}>
-              Filtrar Problemas
-            </Text>
-            <CloseButton
-              onClick={onMenuClose}
-              className={classes.close}
-              aria-label="Close menu"
-            />
-            <Text size="sm" fw={400} className={classes.description} lh={'xs'}>
-              Use os campos abaixo para filtrar os problemas listados. Selecione
-              uma ou mais opções em cada campo e clique em Filtrar para aplicar
-              os filtros.
-            </Text>
+        <FocusTrap>
+          <div className={classes.wrapper}>
+            <div className={classes.header}>
+              <Text size="lg" fw={700} className={classes.title}>
+                Filtrar Problemas
+              </Text>
+              <CloseButton
+                onClick={onMenuClose}
+                className={classes.close}
+                aria-label="Close menu"
+              />
+              <Text
+                size="sm"
+                fw={400}
+                className={classes.description}
+                lh={'xs'}
+              >
+                Use os campos abaixo para filtrar os problemas listados.
+                Selecione uma ou mais opções em cada campo e clique em Filtrar
+                para aplicar os filtros.
+              </Text>
+            </div>
+            <div className={classes.content}>
+              <FocusTrap.InitialFocus />
+              <MultiSelect
+                label="Dificuldade"
+                placeholder="Selecione uma ou mais dificuldades"
+                data={[
+                  'Muito fácil',
+                  'Fácil',
+                  'Intermediário',
+                  'Difícil',
+                  'Muito difícil',
+                ]}
+                classNames={{ pill: classes.pill }}
+                // tabIndex={opened ? 2 : -1}
+              />
+              <MultiSelect
+                label="Tópicos"
+                placeholder="Selecione um ou mais tópicos"
+                data={problemTags}
+                searchable
+                // tabIndex={opened ? 2 : -1}
+              />
+              <MultiSelect
+                label="Status"
+                placeholder="Selecione um ou mais opções"
+                data={['Feito', 'Para fazer', 'Errado']}
+                // tabIndex={opened ? 2 : -1}
+              />
+            </div>
+            <div className={classes.footer}>
+              <Button variant={'lovelace-secondary'}>Limpar</Button>
+              <Button variant={'lovelace-primary'}>Filtrar</Button>
+            </div>
           </div>
-          <div className={classes.content}>
-            <MultiSelect
-              label="Dificuldade"
-              placeholder="Selecione uma ou mais dificuldades"
-              data={[
-                'Muito fácil',
-                'Fácil',
-                'Intermediário',
-                'Difícil',
-                'Muito difícil',
-              ]}
-              classNames={{ pill: classes.pill }}
-            />
-            <MultiSelect
-              label="Tópicos"
-              placeholder="Selecione um ou mais tópicos"
-              data={problemTags}
-              searchable
-            />
-            <MultiSelect
-              label="Status"
-              placeholder="Selecione um ou mais opções"
-              data={['Feito', 'Para fazer', 'Errado']}
-            />
-          </div>
-          <div className={classes.footer}>
-            <Button variant={'lovelace-secondary'}>Limpar</Button>
-            <Button variant={'lovelace-primary'}>Filtrar</Button>
-          </div>
-        </div>
+        </FocusTrap>
       </Popover.Dropdown>
     </Popover>
   );
