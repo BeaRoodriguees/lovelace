4e606e5ddaff5f8c01e82eb373b92dd870657759
Arthur Soares <arthurpmrs@gmail.com>
2024-10-01 10:10:16 -0300

    feat: add api reload

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -4,4 +4,4 @@
 poetry run alembic upgrade head
 
 # Inicia a aplicação
-poetry run uvicorn --host 0.0.0.0 --port 8000 lovelace.app:app
+poetry run uvicorn --host 0.0.0.0 --port 8000 lovelace.app:app --reload
