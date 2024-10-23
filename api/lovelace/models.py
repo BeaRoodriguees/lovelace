f4128c6fefa2c8251039b10aa0e4b6ec4399c6d3
Gabriel Gomes <gomesal2009@gmail.com>
2024-10-23 12:12:05 -0300

    added models

--- api/lovelace/models.py
+++ api/lovelace/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from enum import Enum
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
 
@@ -32,3 +32,40 @@ class User:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
     )
+    sumbissions: Mapped[list['Submission']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Problem:
+    __tablename__ = 'problems'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    description: Mapped[str]
+    problem_input: Mapped[str]
+    problem_output: Mapped[str]
+    sumbissions: Mapped[list['Submission']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Submission:
+    __tablename__ = 'submissions'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped[User] = relationship(init=False, back_populates='submissions')
+    problem_id: Mapped[int] = mapped_column(ForeignKey('problems.id'))
+    problem: Mapped[Problem] = relationship(
+        init=False, back_populates='submissions'
+    )
+    body: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    language: Mapped[str]
+    status: Mapped[str]
+    # updated at?
