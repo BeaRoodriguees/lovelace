412518ec4267b9a72dea299ee2513100ce2b0c5d
Arthur Soares <arthurpmrs@gmail.com>
2024-10-27 15:32:02 -0300

    feat: add some mock entires to database

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -7,5 +7,8 @@ poetry install -q
 # Executa as migrações do banco de dados
 poetry run alembic upgrade head
 
+# Popular banco de dados com dados mockados
+python scripts/populate.py
+
 # Inicia a aplicação
 poetry run uvicorn --host 0.0.0.0 --port 8000 lovelace.app:app --reload
---
+++ api/scripts/populate.py
@@ -0,0 +1,97 @@
+import tomllib
+from pathlib import Path
+from sqlalchemy.orm import Session
+
+from lovelace.models import Difficulty, User, Role, Problem, Tag, TestCase, SubmissonStatus, Submission
+from lovelace.database import engine
+
+def get_users():
+    users = []
+    with open("scripts/data/users.toml", "rb") as file:
+        data = tomllib.load(file)
+        for user in data["users"]:
+            user["role"] = Role(user["role"])
+            users.append(User(**user))
+    
+    return users
+
+
+def get_problems():
+    folder = Path('scripts/data/problems')
+    problems = []
+
+    for filename in folder.iterdir():
+        if filename.suffix == ".toml":
+            with open(folder / filename.name, "rb") as data:
+                problem = tomllib.load(data)
+                problem["difficulty"] = Difficulty(problem["difficulty"])
+                problem["testcases"] = [TestCase(**testcase) for testcase in problem["testcases"]]
+                problem["tags"] = [Tag(**tag) for tag in problem["tags"]]
+                
+                problems.append(Problem(**problem))
+    
+    return problems
+
+
+def get_and_add_submissions(session):
+    if session.query(Problem).count() == 0 and session.query(User).count() == 0:
+        return 
+
+    submissions = []
+    with open("scripts/data/submissions.toml", "rb") as file:
+        data = tomllib.load(file)
+        for submission in data["submissions"]:
+            submission["status"] = SubmissonStatus(submission["status"])
+            problem_id = session.query(Problem.id).filter(Problem.name == submission["problem_name"]).first()[0]
+            submission.pop('problem_name')
+            user_id = session.query(User.id).filter(User.username == submission["username"]).first()[0]
+            submission.pop("username")
+
+            submissions.append(Submission(**submission, problem_id=problem_id, user_id=user_id))
+
+    session.add_all(submissions)
+
+def is_populated():
+    with Session(engine) as session:
+        return session.query(Problem).count() > 0
+
+def populate():
+    users = get_users()
+    problems = get_problems()
+
+    with Session(engine) as session:
+        # Add all tags
+        existing_tags = set([tag.name for tag in session.query(Tag).all()])
+        inserting_tags = []
+        for problem in problems:
+            inserting_tags.extend([tag.name for tag in problem.tags])
+        
+        tags = set(inserting_tags) - existing_tags
+
+        session.add_all([Tag(name=tag) for tag in tags])
+
+        # Pull problem tags fro mdatabase and add them to the problem
+        for i, problem in enumerate(problems):
+            tags_names = [tag.name for tag in problem.tags]
+            problems[i].tags = session.query(Tag).filter(Tag.name.in_(tags_names)).all()
+
+        # Add all problems
+        session.add_all(problems)
+
+        # Add all users
+        session.add_all(users)
+
+        # Add all submissions
+        get_and_add_submissions(session)
+        
+        session.commit()
+
+
+
+if __name__ == '__main__':
+    if is_populated():
+        print("Database is already populated.")
+    else:
+        print("Populating development database with mock entries...")
+        populate()
+        print("Populating process finished.")
