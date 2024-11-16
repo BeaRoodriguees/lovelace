b0eb892996d4b33b1edb4df469825d65275487b0
Davi Romão <davi.romao0210@gmail.com>
2024-11-16 20:07:26 -0300

    fix: entrypoint with poetry

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -8,7 +8,7 @@ poetry install -q
 poetry run alembic upgrade head
 
 # Popular banco de dados com dados mockados
-python scripts/populate.py
+poetry run python scripts/populate.py
 
 # Inicia a aplicação
 poetry run uvicorn --host 0.0.0.0 --port 8000 lovelace.app:app --reload
